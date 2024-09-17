25d78c1d17b749e278a46b10410c2ee1067e0e9f
Juan Rogelio Franco Martinez <juanframart2011@gmail.com>
2024-09-16 22:09:46 -0600

    form request of auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\AuthRequest;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(AuthRequest $request)
     {
         $credentials = $request->only('username', 'password');
 
---
+++ app/Http/Requests/AuthRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'username.required' => 'El campo de usuario es obligatorio.',
+            'password.required' => 'El campo de contraseña es obligatorio.',
+        ];
+    }
+}
